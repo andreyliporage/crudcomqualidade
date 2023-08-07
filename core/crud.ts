221b175c43a99b56f3110a84c6a1d0ab59a9d535
andreyliporage <andreyliporage@gmail.com>
2023-08-07 17:00:39 -0300

    ajuste lint, prettier e eslint

--- core/crud.ts
+++ core/crud.ts
@@ -1,30 +1,39 @@
 import fs from "fs";
+import { v4 as uuid } from "uuid";
+
 const DB_FILE_PATH = "./core/db";
 
 console.log("[CRUD]");
 
 interface Todo {
-    date: string,
-    content: string,
-    done: boolean
-};
+    id: string;
+    date: string;
+    content: string;
+    done: boolean;
+}
 
-function create(content: string) {
+function create(content: string): Todo {
     const todo: Todo = {
+        id: uuid(),
         date: new Date().toISOString(),
         content: content,
-        done: false
+        done: false,
     };
 
-    const todos: Array<Todo> = [
-        ...read(),
-        todo,
-    ]
+    const todos: Array<Todo> = [...read(), todo];
 
-    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
-        todos,
-    }, null, 2));
-    return content;
+    fs.writeFileSync(
+        DB_FILE_PATH,
+        JSON.stringify(
+            {
+                todos,
+            },
+            null,
+            2
+        )
+    );
+
+    return todo;
 }
 
 function read(): Array<Todo> {
@@ -33,8 +42,60 @@ function read(): Array<Todo> {
     if (!db.todos) {
         return [];
     }
-    
+
     return db.todos;
+}
+
+function update(id: string, partialTodo: Partial<Todo>): Todo {
+    let updatedTodo;
+    const todos = read();
+
+    todos.forEach((currentTodo) => {
+        const isToUpdate = currentTodo.id === id;
+        if (isToUpdate) {
+            updatedTodo = Object.assign(currentTodo, partialTodo);
+        }
+    });
+
+    fs.writeFileSync(
+        DB_FILE_PATH,
+        JSON.stringify(
+            {
+                todos,
+            },
+            null,
+            2
+        )
+    );
+
+    if (!updatedTodo) {
+        throw new Error("Please, provide another ID!");
+    }
+
+    return updatedTodo;
+}
+
+function deleteById(id: string) {
+    const todos = read();
+
+    const todosWithoutOne = todos.filter((todo) => {
+        if (id === todo.id) {
+            return false;
+        }
+
+        return true;
+    });
+
+    fs.writeFileSync(
+        DB_FILE_PATH,
+        JSON.stringify(
+            {
+                todos: todosWithoutOne,
+            },
+            null,
+            2
+        )
+    );
 }
 
 function CLEAR_DB() {
@@ -44,5 +105,9 @@ function CLEAR_DB() {
 // [SIMULATION]
 CLEAR_DB();
 create("Primeira TODO");
+const terceiraTodo = create("Segunda TODO");
+update(terceiraTodo.id, {
+    content: "Atualizada!",
+});
 create("Segunda TODO");
 console.log(read());
